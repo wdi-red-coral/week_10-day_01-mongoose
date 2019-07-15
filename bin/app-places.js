ca05caf8f148908d444601f65521ec7f2a8bbe17
AmanyAts <amanyalattass@gmail.com>
2019-07-15 10:00:37 +0300

    mongoose Homework

--- bin/app-places.js
+++ bin/app-places.js
@@ -6,6 +6,7 @@ mongoose.connect('mongodb://localhost/mongoose-crud', {
   useMongoClient: true
 })
 const db = mongoose.connection
+const Place = require('../models/place.js')
 
 const done = function () { // eslint-disable-line no-unused-vars
   db.close()
@@ -14,22 +15,57 @@ const done = function () { // eslint-disable-line no-unused-vars
 // CRUD Actions
 const create = function (name, latitude, longitude, country) {
   /* Add Code Here */
+  const placeParams={
+    name:name,
+    latitude:latitude,
+    longitude:longitude,
+    country:country
+  }
+  Place.create(placeParams)
+  .then(place=>console.log(place.toJSON()))
+  .catch(console.error)
+  .then(done)
+
 }
 
 const index = function () {
   /* Add Code Here */
+  Place.find()
+  .then((places)=>{
+    places.forEach(place=> console.log(place.toJSON()))
+  })
+  .catch(console.error)
+  .then(done)
 }
 
 const show = function (id) {
   /* Add Code Here */
+  Place.findById(id)
+  .then(place=>console.log(place.toJSON()))
+  .catch(console.error)
+  .then(done)
 }
 
 const update = function (id, field, value) {
   /* Add Code Here */
+  Place.findById(id)
+  .then(place=>{
+    place[field]=value
+    return place.save()
+
+  })
+  .catch(console.error)
+  .then(done)
 }
 
 const destroy = function (id) {
   /* Add Code Here */
+  Place.findById(id)
+  .then(place=>{
+    place.remove()
+  })
+  .catch(console.error)
+  .then(done)
 }
 
 // UI
--- models/person.js
+++ models/person.js
@@ -1,1 +1,47 @@
-'use strict'
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema(
+  {
+
+    name:{
+      firstName:{
+        type:String,
+        required:true
+      },
+      lastName:{
+        type:String,
+        required:true
+      }
+    },
+    height:{
+      type:Number,
+      required: true,
+      min:0
+    },
+    weight:{
+      type:Number,
+      required:true,
+      min:0
+    }
+},
+
+{
+  timestamps:true,
+  toObject:{
+    virtuals:true
+  },
+  toJSON:{
+    virtuals:true
+  }
+
+}
+
+);
+
+personSchema.virtual('name.full').get(function(){
+  return this.name.firstName+ ''+ this.name.lastName
+})
+//model
+const Person= mongoose.model('Person', personSchema)
+
+module.exports= Person
