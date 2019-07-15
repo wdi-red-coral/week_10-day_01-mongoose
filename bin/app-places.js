8c7ce37664da609cbf2b174e68bc1f748b566bf7
asmamk <i.amk94@hotmail.com>
2019-07-15 15:48:00 +0300

    add some tiles

--- bin/app-places.js
+++ bin/app-places.js
@@ -6,6 +6,7 @@ mongoose.connect('mongodb://localhost/mongoose-crud', {
   useMongoClient: true
 })
 const db = mongoose.connection
+const Pleace = require('../models/pleaces.js')
 
 const done = function () { // eslint-disable-line no-unused-vars
   db.close()
@@ -14,22 +15,58 @@ const done = function () { // eslint-disable-line no-unused-vars
 // CRUD Actions
 const create = function (name, latitude, longitude, country) {
   /* Add Code Here */
+  const pleaceParams={
+    name : name,
+    latitude:latitude ,
+    longitude :longitude,
+    country:country
+  }
+  Pleace.create(pleaceParams)
+  .then(pleace=>console.log(pleace.toJSON()))
+  .catch(console.error)
+  .then(done)
+
 }
 
 const index = function () {
   /* Add Code Here */
+
+  Pleace.find()
+  .then((pleace)=>{
+    pleace.forEach(pleace => console.log(pleace.toJSON()));
+  })
+  .catch(console.error)
+  .then(done)
 }
 
 const show = function (id) {
   /* Add Code Here */
+  Pleace.findById(id)
+  .then(pleace => console.log(pleace.toJSON()))
+  .catch(console.error)
+  .then(done)
 }
 
 const update = function (id, field, value) {
   /* Add Code Here */
+
+  Pleace.findById(id)
+  .then(pleace => {
+    pleace[field] = value
+    return pleace.save()
+  })
+  .then(pleace => console.log(pleace.toJSON()))
+  .catch(console.error)
+  .then(done)
 }
 
 const destroy = function (id) {
   /* Add Code Here */
+
+  Please.findById(id)
+  .then(please => please.remove())
+  .catch(console.error)
+  .then(done)
 }
 
 // UI
