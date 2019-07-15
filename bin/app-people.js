1e22e4826dd6664931d2b70841fc3c8c82a22656
saeedalzahrani <saeedalzahrani103@gmail.com>
2019-07-15 09:53:21 +0300

    HW-sunday

--- bin/app-people.js
+++ bin/app-people.js
@@ -7,72 +7,111 @@ mongoose.connect('mongodb://localhost/mongoose-crud', {
 })
 const db = mongoose.connection
 
-const Person = require('../models/person.js')
-
+const Place = require('../models/place.js')
 const done = function () { // eslint-disable-line no-unused-vars
   db.close()
 }
 
-const index = function () {
+// CRUD Actions
+const create = function (name, latitude, longitude, country) {
   /* Add Code Here */
+  const placeParams = {
+    name,
+    latitude,
+    longitude,
+    country
+  }
+  Place.create(placeParams)
+   .then(
+     place => console.log(place),
+    error => console.error
+   )
+  .then(done)
+}
+
+const index = function () {
+ /* Add Code Here */
+ Place.find()
+ .then(
+   places => places.forEach(place => console.log(place)),
+   error => console.error
+ )
+ .then(done)
 }
 
 const show = function (id) {
-  /* Add Code Here */
-}
-
-const destroy = function (id) {
-  /* Add Code Here */
+ /* Add Code Here */
+ Place.findById(id)
+ .then(
+   place => console.log(place.toJSON()),
+   error => console.error
+ )
+ .then(done)
 }
 
 const update = function (id, field, value) {
-  /* Add Code Here */
+ /* Add Code Here */
+Place.findById(id)
+.then(
+  place => {
+    place[field] = value;
+    return place.save()
+  },
+  error => console.error
+)
+.then(done)
 }
 
-const create = function (firstName, lastName, dob, height, weight) {
-  /* Add Code Here */
+
+const destroy = function (id) {
+ /* Add Code Here */
+ Place.findById(id)
+ .then(
+   place => place.remove(),
+   error => console.error
+ )
+ .then(done)
 }
 
+// UI
 db.once('open', function () {
-  const command = process.argv[2]
+ const command = process.argv[2]
 
-  let field
-  let id
+ let field
+ let id
 
-  switch (command) {
-    case 'create':
-      const firstName = process.argv[3]
-      const lastName = process.argv[4]
-      const dob = process.argv[5]
-      const height = process.argv[6]
-      const weight = process.argv[7]
+ switch (command) {
+   case 'create':
+     const name = process.argv[3]
+     const latitude = process.argv[4]
+     const longitude = process.argv[5]
+     const country = process.argv[6]
 
-      create(firstName, lastName, dob, height, weight)
+     create(name, latitude, longitude, country)
 
-      break
+     break
 
-    case 'show':
-      id = process.argv[3]
-      show(id)
-      break
+   case 'show':
+     id = process.argv[3]
+     show(id)
+     break
 
-    case 'update':
-      id = process.argv[3]
-      field = process.argv[4]
-      const value = process.argv[5]
-      update(id, field, value)
-      break
+   case 'update':
+     id = process.argv[3]
+     field = process.argv[4]
+     const value = process.argv[5]
+     update(id, field, value)
+     break
 
-    case 'destroy':
-      id = process.argv[3]
-      destroy(id)
-      break
+   case 'destroy':
+     id = process.argv[3]
+     destroy(id)
+     break
 
     default:
       index()
-
-      break
+    break
   }
 })
 
-module.exports = Person
+module.exports = place;
