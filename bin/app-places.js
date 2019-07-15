81e50e6bda532e2c5179aafa9f52c34b439dbb8b
akhouja1994 <a.khouja1994@gmail.com>
2019-07-15 10:04:28 +0300

    HW

--- bin/app-places.js
+++ bin/app-places.js
@@ -1,74 +1,115 @@
-'use strict'
-
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/mongoose-crud', {
-  useMongoClient: true
+ useMongoClient: true
 })
 const db = mongoose.connection
 
+const Place = require('../models/place.js')
 const done = function () { // eslint-disable-line no-unused-vars
-  db.close()
+ db.close()
 }
 
 // CRUD Actions
 const create = function (name, latitude, longitude, country) {
-  /* Add Code Here */
+ /* Add Code Here */
+ const placeParams = {
+   name,
+   latitude,
+   longitude,
+   country
+ }
+ Place.create(placeParams)
+ .then(
+   place => console.log(place),
+   error => console.error
+ )
+ .then(done)
 }
 
 const index = function () {
-  /* Add Code Here */
+ /* Add Code Here */
+ Place.find()
+ .then(
+   places => places.forEach(place => console.log(place)),
+   error => console.error
+ )
+ .then(done)
 }
 
 const show = function (id) {
-  /* Add Code Here */
+ /* Add Code Here */
+ Place.findById(id)
+ .then(
+   place => console.log(place.toJSON()),
+   error => console.error
+ )
+ .then(done)
 }
 
 const update = function (id, field, value) {
-  /* Add Code Here */
+ /* Add Code Here */
+ Place.findById(id)
+ .then(
+   place => {
+     place[field] = value;
+     return place.save()
+   },
+   error => console.error
+ )
+ .then(done)
 }
 
+
 const destroy = function (id) {
-  /* Add Code Here */
+ /* Add Code Here */
+ Place.findById(id)
+ .then(
+   place => place.remove(),
+   error => console.error
+ )
+ .then(done)
 }
 
 // UI
 db.once('open', function () {
-  const command = process.argv[2]
+ const command = process.argv[2]
 
-  let field
-  let id
+ let field
+ let id
 
-  switch (command) {
-    case 'create':
-      const name = process.argv[3]
-      const latitude = process.argv[4]
-      const longitude = process.argv[5]
-      const country = process.argv[6]
+ switch (command) {
+   case 'create':
+     const name = process.argv[3]
+     const latitude = process.argv[4]
+     const longitude = process.argv[5]
+     const country = process.argv[6]
 
-      create(name, latitude, longitude, country)
+     create(name, latitude, longitude, country)
 
-      break
+     break
 
-    case 'show':
-      id = process.argv[3]
-      show(id)
-      break
+   case 'show':
+     id = process.argv[3]
+     show(id)
+     break
 
-    case 'update':
-      id = process.argv[3]
-      field = process.argv[4]
-      const value = process.argv[5]
-      update(id, field, value)
-      break
+   case 'update':
+     id = process.argv[3]
+     field = process.argv[4]
+     const value = process.argv[5]
+     update(id, field, value)
+     break
 
-    case 'destroy':
-      id = process.argv[3]
-      destroy(id)
-      break
+   case 'destroy':
+     id = process.argv[3]
+     destroy(id)
+     break
 
-    default:
-      index()
-      break
-  }
+   default:
+     index()
+     break
+ }
 })
+
+module.exports = Place;
