6a3748970365c4f6b48229096ccc1ec5c412d62b
hudabz <huda_bz@hotmail.com>
2019-07-15 21:47:48 +0300

    HW week 10 day 1, I have an error on the  virtuals

--- bin/app-places.js
+++ bin/app-places.js
@@ -7,6 +7,8 @@ mongoose.connect('mongodb://localhost/mongoose-crud', {
 })
 const db = mongoose.connection
 
+const Place = require("../models/place.js")
+
 const done = function () { // eslint-disable-line no-unused-vars
   db.close()
 }
@@ -14,22 +16,63 @@ const done = function () { // eslint-disable-line no-unused-vars
 // CRUD Actions
 const create = function (name, latitude, longitude, country) {
   /* Add Code Here */
+
+  const placeParams ={
+    name:name,
+    latitude:latitude, 
+    longitude:longitude, 
+    country:country
+  
+  } 
+  Place.create(placeParams) 
+  .then(place => console.log(place))
+  .catch(console.error)
+  .then(done); 
 }
 
 const index = function () {
   /* Add Code Here */
+
+  Place.find() 
+  .then((places) => {
+    places.forEach((place)=> console.log(place))
+   })
+ 
+  .catch(console.error)
+  .then(done);
 }
 
 const show = function (id) {
   /* Add Code Here */
+
+  Place.findById(id) 
+  .then(place => console.log(place)) 
+  .catch(console.error)
+  .then(done);
 }
 
 const update = function (id, field, value) {
   /* Add Code Here */
+
+  Place.findById(id) 
+  .then((place) => { 
+    place[field] =value 
+    return place.save()
+  })
+  .then(place => console.log(place))
+  .catch(console.error)
+  .then(done);
 }
 
 const destroy = function (id) {
   /* Add Code Here */
+
+  Place.findById(id) 
+  .then(place => {
+    place.remove()
+  })
+  .catch(console.error)
+  .then(done);
 }
 
 // UI
@@ -71,4 +114,7 @@ db.once('open', function () {
       index()
       break
   }
-})
+}) 
+
+module.exports = Place
+
