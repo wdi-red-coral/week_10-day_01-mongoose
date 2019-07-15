6504d693737316b9f761041600e80339dd372b83
oalmalky5 <oalmalky5@gmail.com>
2019-07-15 09:33:02 +0300

    HW

--- bin/app-places.js
+++ bin/app-places.js
@@ -7,6 +7,9 @@ mongoose.connect('mongodb://localhost/mongoose-crud', {
 })
 const db = mongoose.connection
 
+const Person = require('../models/place.js')
+
+
 const done = function () { // eslint-disable-line no-unused-vars
   db.close()
 }
@@ -14,23 +17,66 @@ const done = function () { // eslint-disable-line no-unused-vars
 // CRUD Actions
 const create = function (name, latitude, longitude, country) {
   /* Add Code Here */
+  const placeParams = {
+    name: name,
+    latitude : latitude,
+    longitude : longitude,
+    country : country
+
+    
+  }
+  Place.create(placeParams)
+  .then((place) => console.log(place.toJSON()))
+  .catch(console.log(console.error))
+  .then(done)
+
+  
 }
 
 const index = function () {
   /* Add Code Here */
+  Place.find()
+  .then((places) => {
+    places.forEach(place => console.log(place.toJSON()))
+  })
+  .catch(console.log(console.error))
+  .then(done)
 }
+  
 
 const show = function (id) {
   /* Add Code Here */
+  Place.findById(id)
+    .then((place) => console.log(place.toJSON())
+    )
+    .catch(console.log(console.error))
+    .then(done)
 }
 
 const update = function (id, field, value) {
   /* Add Code Here */
+
+  Place.findById(id)
+  .then((place) => {
+    place[field] = value
+    return place.save()
+  })
+  .then((place) => console.log(place.toJSON()))
+  .catch(console.log(console.error))
+  .then(done)
+
+  
 }
 
 const destroy = function (id) {
-  /* Add Code Here */
+  /* Add Code Here */  
+  Place.findById(id)
+    .then((place) => place.remove())
+    .catch(console.log(console.error))
+    .then(done)
 }
+
+
 
 // UI
 db.once('open', function () {
---
+++ models/place.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    latitude: {
+        type: Number,
+        required: true,
+        min: -90,
+        max: 90
+      },
+      longitude: {
+        type: Number,
+        required: true,
+        min: -180,
+        max: 180
+      },
+      conutry: {
+          type: String
+          
+        }
+    },
+    {
+        timestamps: true,
+        toObject: {
+            virtuals: true,
+        },
+        toJSON: {
+            virtuals: true,
+        }
+      
+})
+
+placeSchema.virtual('isNorthernHemisphere?').get(function () {
+    return this.latitude > 0
+  })
+  
+  placeSchema.virtual('isWesternHemisphere?').get(function () {
+    return this.longitude < 0
+  })
+
+  const Place = mongoose.model('Place', placeSchema)
+
+module.exports = Place
